328f3a53838625d5a5fe5347fb6f202aba7af5ef
avikavg-web <avikavg@gmail.com>
2025-12-31 20:09:32 -0800

    Homwork

---
+++ Homework.py
@@ -0,0 +1,108 @@
+thing = input("Write anything and I will identify if it is a letter: ")
+
+if thing == "a":
+    print("yes")
+elif thing == "b":
+    print("yes")
+elif thing == "c":
+    print("yes")
+elif thing == "d":
+    print("yes")
+elif thing == "e":
+    print("yes")
+elif thing == "f":
+    print("yes")
+elif thing == "g":
+    print("yes")
+elif thing == "h":
+    print("yes")
+elif thing == "i":
+    print("yes")
+elif thing == "j":
+    print("yes")
+elif thing == "k":
+    print("yes")
+elif thing == "l":
+    print("yes")
+elif thing == "m":
+    print("yes")
+elif thing == "n":
+    print("yes")
+elif thing == "o":
+    print("yes")
+elif thing == "p":
+    print("yes")
+elif thing == "q":
+    print("yes")
+elif thing == "r":
+    print("yes")
+elif thing == "s":
+    print("yes")
+elif thing == "t":
+    print("yes")
+elif thing == "u":
+    print("yes")
+elif thing == "v":
+    print("yes")
+elif thing == "w":
+    print("yes")
+elif thing == "x":
+    print("yes")
+elif thing == "y":
+    print("yes")
+elif thing == "z":
+    print("yes")
+elif thing == "A":
+    print("yes")
+elif thing == "B":
+    print("yes")
+elif thing == "C":
+    print("yes")
+elif thing == "D":
+    print("yes")
+elif thing == "E":
+    print("yes")
+elif thing == "F":
+    print("yes")
+elif thing == "G":
+    print("yes")
+elif thing == "H":
+    print("yes")
+elif thing == "I":
+    print("yes")
+elif thing == "J":
+    print("yes")
+elif thing == "K":
+    print("yes")
+elif thing == "L":
+    print("yes")
+elif thing == "M":
+    print("yes")
+elif thing == "N":
+    print("yes")
+elif thing == "O":
+    print("yes")
+elif thing == "P":
+    print("yes")
+elif thing == "Q":
+    print("yes")
+elif thing == "R":
+    print("yes")
+elif thing == "S":
+    print("yes")
+elif thing == "T":
+    print("yes")
+elif thing == "U":
+    print("yes")
+elif thing == "V":
+    print("yes")
+elif thing == "W":
+    print("yes")
+elif thing == "X":
+    print("yes")
+elif thing == "Y":
+    print("yes")
+elif thing == "Z":
+    print("yes")
+else:
+    print("no") 
