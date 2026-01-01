816139495737fab3f326fa8726e678a3e269c98c
avikavg-web <avikavg@gmail.com>
2025-12-31 19:39:35 -0800

    Lesson 6

---
+++ Activity2.py
@@ -0,0 +1,30 @@
+a = 12
+b = 12
+c = 5
+
+print(a != b)
+print(b != c)
+
+d = "Hi"
+e = "Good"
+
+if d == e:
+    print("These two words are the same")
+else:
+    print("These words are not the same")
+
+f = 3
+g = 74
+
+if(f == 3) != (g == 74):
+    print("Yes")
+else:
+    print("No")
+
+number = int(input("Give me a number"))
+
+if (number % 2) == 0:
+    print("ok")
+else:
+    print("no")
+    
